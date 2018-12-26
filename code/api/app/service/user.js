0abb8ddee2b8509b0667b56637e97cc423c97cd9
PengDon <l2j9y7d2>
2018-12-26 22:30:17 +0800

    过滤掉不想返回的字段

--- code/api/app/service/user.js
+++ code/api/app/service/user.js
@@ -5,11 +5,11 @@ const Service = require('egg').Service;
 class HomeService extends Service {
   // 查询用户列表
   async queryUserList() {
-    return this.ctx.model.User.find();
+    return this.ctx.model.User.find({},{'password':0});
   }
   // 根据name查询用户
   async queryUserByName(param) {
-    return this.ctx.model.User.find(param);
+    return this.ctx.model.User.find(param,{'password':0});
   }
 }
 
