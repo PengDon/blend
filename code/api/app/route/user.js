96258250bf7f0040b7232668673683298a9f84e2
tangp <tangp@dtds.com.cn>
2018-12-27 13:51:09 +0800

    优化接口格式

--- code/api/app/route/user.js
+++ code/api/app/route/user.js
@@ -1,10 +1,10 @@
 module.exports = app => {
   // 查询所有用户信息
-  app.router.get('/getUserList', app.controller.user.list);
-  // 根据name查询用户
-  // app.router.get('/user/:name', app.controller.user.getUserByName);
-
-  app.router.post('/user', app.controller.user.login);
+  app.router.get('/api/v1/getUserList', app.controller.user.list);
+  //根据name查询用户
+  app.router.get('/user/:name', app.controller.user.getUserByName);
+  // 用户登录
+  app.router.post('/api/v1/login', app.controller.user.login);
 
 
 };
