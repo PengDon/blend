01df6fac0bd00e3b71c6b89c59e3b39f967f2b8e
dontang <dontang@hinabian.com>
2020-01-14 11:18:58 +0800

    合并

--- code/api/app/routers/home.js
+++ code/api/app/routers/home.js
@@ -1,11 +1,7 @@
 module.exports = app => {
   const { router, controller } = app;
-<<<<<<< HEAD
   // auth中间件的配置方法二，与方法一取其一
   // const auth = app.middleware.auth(); // 引入auth中间件,括号内可以传递参数给中间件{key: value}
   // router.get("/home", auth, controller.home.index); // 只在/home路由使用，放到第二个参数
   router.get("/home", controller.home.index); // 只在/home路由使用，放到第二个参数
-=======
-  router.get("/home", controller.home.index);
->>>>>>> c96ec236e7735281f8d58734b60d2a36bcdfbb37
 };
