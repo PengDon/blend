0c96c28f958698bcf1caa3b9b79ee9fc7f7b1deb
dontang <dontang@hinabian.com>
2020-02-27 14:57:15 +0800

    删除无效代码

--- code/vue3-ts/src/store/view.ts
+++ code/vue3-ts/src/store/view.ts
@@ -1,9 +1,7 @@
 import { MutationTree, ActionTree } from 'vuex'
 
 export interface IViewState {
-  sidebarOpened: boolean
   title: string
-  menuList: Array<{ [propsName: string]: any }>
 }
 
 export interface RootState {
@@ -11,49 +9,25 @@ export interface RootState {
 }
 
 export enum TYPES {
-  OPEN_SIDEBAR = 'OPEN_SIDEBAR', // 打开侧边栏
-  CLOSE_SIDEBAR = 'CLOSE_SIDEBAR', // 关闭侧边栏
-  SET_TITLE = 'SET_TITLE',
-  SET_MENU = 'SET_MENU' // 设置菜单
+  SET_TITLE = 'SET_TITLE'
 }
 
 const state: IViewState = {
-  sidebarOpened: true,
-  title: '主页',
-  menuList: []
+  title: '主页'
 }
 
 const getters = {
   // tslint:disable-next-line:no-shadowed-variable
-  sidebarOpened: (state: IViewState) => state.sidebarOpened
 }
 
 const mutations: MutationTree<IViewState> = {
-  [TYPES.OPEN_SIDEBAR](s: IViewState) {
-    s.sidebarOpened = true
-  },
-  [TYPES.CLOSE_SIDEBAR](s: IViewState) {
-    s.sidebarOpened = false
-  },
   [TYPES.SET_TITLE](s: IViewState, title: string) {
     s.title = title
-  },
-  [TYPES.SET_MENU]: (
-    s: IViewState,
-    payload: Array<{ [propsName: string]: any }>
-  ) => {
-    s.menuList = payload
   }
 }
 
 const actions: ActionTree<IViewState, RootState> = {
   // tslint:disable-next-line:no-shadowed-variable
-
-  toogleSidebar({ state, commit }) {
-    state.sidebarOpened
-      ? commit(TYPES.CLOSE_SIDEBAR)
-      : commit(TYPES.OPEN_SIDEBAR)
-  },
 
   setTitle({ commit }, params) {
     commit(TYPES.SET_TITLE, params)
