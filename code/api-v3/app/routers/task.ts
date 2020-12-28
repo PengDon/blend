e7e469884fdd21f9228184621bc8c0f597f9e504
PengDon <xxadress@sina.com>
2020-12-28 17:00:09 +0800

    更新单元测试

--- code/api-v3/app/routers/task.ts
+++ code/api-v3/app/routers/task.ts
@@ -2,15 +2,21 @@
  * @Author: don
  * @Date: 2020-12-28 13:49:58
  * @LastEditors: don
- * @LastEditTime: 2020-12-28 16:42:32
+ * @LastEditTime: 2020-12-28 16:58:35
  * @Description:
  */
 module.exports = app => {
   const { controller, router } = app;
-
+  // 查询所有
   router.get('/task', controller.task.index);
+  // 根据id查询
   router.get('/task/:id', controller.task.show);
+  // 新增任务
   router.post('/task', controller.task.create);
+  // 修改部分数据
   router.patch('/task/:id', controller.task.update);
+  // 根据id修改所有数据
+  router.put('/task/:id', controller.task.update);
+  // 根据id删除任务
   router.delete('/task/:id', controller.task.destroy);
 };
--- code/api-v3/test/app/controller/task.test.ts
+++ code/api-v3/test/app/controller/task.test.ts
@@ -2,7 +2,7 @@
  * @Author: don
  * @Date: 2020-12-28 13:57:15
  * @LastEditors: don
- * @LastEditTime: 2020-12-28 16:49:31
+ * @LastEditTime: 2020-12-28 16:59:08
  * @Description:
  */
 
@@ -27,19 +27,29 @@ describe('test/app/controller/task.test.ts', () => {
     assert(result.body.data.length > 0);
   });
 
-  it(`should PATCH /api/v1/admin/task/:id update task`, async () => {
+  it('should PATCH /api/v1/admin/task/:id update task', async () => {
     const result = await app.httpRequest().patch(`/api/v1/admin/task/${id}`).send({
       name: 'webpack源码解读'
     }).expect(200);
     assert(result.body.msg === '修改成功');
   });
 
-  it(`should GET /api/v1/admin/task/:id query one`, async () => {
+  it('should GET /api/v1/admin/task/:id query one', async () => {
     const result = await app.httpRequest().get(`/api/v1/admin/task/${id}`).expect(200);
     assert(result.body.data.name === 'webpack源码解读');
   });
 
-  it(`should DELETE /api/v1/admin/task/:id delete task`, async () => {
+  it('should PUT /api/v1/admin/task/:id put task', async () => {
+    const result = await app.httpRequest().put(`/api/v1/admin/task/${id}`).send({
+      name: 'vuex vs mobx',
+      userName: 'don',
+      status: '进行中',
+      createTime: '20201223'
+    }).expect(200);
+    assert(result.body.msg === '修改成功');
+  });
+
+  it('should DELETE /api/v1/admin/task/:id delete task', async () => {
     const result = await app.httpRequest().delete(`/api/v1/admin/task/${id}`).expect(200);
     assert(result.body.msg === '删除成功');
   });
